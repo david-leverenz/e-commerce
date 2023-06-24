ae0c1a0e3b92af5db06c841d5460c4dde92d0496
david-leverenz <david.leverenz@gmail.com>
2023-06-24 17:30:58 -0500

    seed file created but throw errors

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  onDelete: 'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'driver_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
