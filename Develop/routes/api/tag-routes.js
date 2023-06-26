d150c968d102a6dcd679095dd73642344f76d829
david-leverenz <david.leverenz@gmail.com>
2023-06-26 17:39:17 -0500

    find all tags route working, I think

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,34 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'tags_product' }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
+// router.get('/:id', (req, res) => {
+//   // find a single tag by its `id`
+//   // be sure to include its associated Product data
+// });
 
-router.post('/', (req, res) => {
-  // create a new tag
-});
+// router.post('/', (req, res) => {
+//   // create a new tag
+// });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+// router.put('/:id', (req, res) => {
+//   // update a tag's name by its `id` value
+// });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+// router.delete('/:id', (req, res) => {
+//   // delete on tag by its `id` value
+// });
 
 module.exports = router;
