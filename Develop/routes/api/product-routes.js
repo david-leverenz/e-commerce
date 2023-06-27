ff202791ec176957c12ecbb72b9c3c1ec5443b64
david-leverenz <david.leverenz@gmail.com>
2023-06-27 10:10:40 -0500

    cleaned files out

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -37,49 +37,6 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-//  try {
-//   const productData = await Product.create(req.body);
-//   res.status(200).json(productData);
-// } catch (err) {
-//   res.status(400).json(err);
-// }
-
-// create new product - getting an error with this one.  It doesn't like line 57 which, I believe, was written for us.  I can't figure it out.  Moving on...
-// router.post('/', async (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-
-// // });
-// }
-  // try {
-  //   const productData = await Product.create(req.body)
-  //   .then((product) => {
-  //     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-  //     if (req.body.tagIds.length) {
-  //       const productTagIdArr = req.body.tagIds.map((tag_id) => {
-  //         return {
-  //           product_id: product.id,
-  //           tag_id,
-  //         };
-  //       });
-  //       return ProductTag.bulkCreate(productTagIdArr);
-  //     }
-  //     // if no product tags, just respond
-  //     res.status(200).json(product);
-  //   })
-  //   .then((productTagIds) => res.status(200).json(productTagIds))
-  //   } catch(err) {
-  //     console.log(err);
-  //     res.status(400).json(err);
-  //   }
-  // });
-
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
